659e2218c8992de009ec01b119784cec7952154b
Lyrical <chrisblum@gmail.com>
2021-12-07 10:01:23 -0800

    A little refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,118 +1,28 @@
 mod submarine;
 
-use std::fs::File;
-use std::io::{self, BufRead};
-use std::path::Path;
+
 use crate::submarine::Submarine;
 
 const DEPTH_DATA_FILE: &str = "./sea_depth_data.txt";
 const SUBMARINE_MOVEMENT_DATA_FILE: &str = "./submarine_movement_data.txt";
 const SUB_BINARY_DIAGNOSTIC_DATA_FILE: &str = "./binary_diagnostic_data.txt";
-//const SUB_BINARY_DIAGNOSTIC_DATA_FILE: &str = "./test_data.txt";
+const SUB_TEST_DIAGNOSTIC_DATA_FILE: &str = "./test_diag_data.txt";
+const SUB_TEST_BINGO_DATA_FILE: &str = "test_bingo_data.txt";
 
 fn main() {
-    let my_sub = move_sub(submarine::Submarine::new());
+    let mut my_sub = submarine::Submarine::new();
+    my_sub.move_sub(SUBMARINE_MOVEMENT_DATA_FILE);
     // Day 1
-    println!("Number of measurements that are larger than the previous measurement: {}", get_increased_depth());
+    println!("Number of measurements that are larger than the previous measurement: {}", my_sub.get_increased_depth(DEPTH_DATA_FILE));
 
     // Day 2
     println!("Final horizontal position times depth: {}",  my_sub.horizontal_position * my_sub.depth);
 
-    let mut my_sub = process_diagnostics(my_sub);
+    my_sub.process_diagnostics(SUB_BINARY_DIAGNOSTIC_DATA_FILE);
     // Day 3
     println!("My diagnostics and count: {:?}, {}", my_sub.diag_distribution, my_sub.diagnostic_count);
     println!("My power consumption: {}", my_sub.get_power_consumption());
     println!("My life support rating: {}", my_sub.get_life_support_rating());
-}
-
-fn process_diagnostics(mut uboat: Submarine) -> Submarine {
-    if let Ok(lines) = read_lines(SUB_BINARY_DIAGNOSTIC_DATA_FILE) {
-        for line in lines {
-            if let Ok(bits) = line {
-                let char_vec: Vec<char> = bits.chars().collect();
-                uboat.store_diagnostics(char_vec);
-            }
-        }
-    }
-    uboat
-}
-
-fn move_sub(mut uboat: Submarine) -> Submarine {
-    if let Ok(lines) = read_lines(SUBMARINE_MOVEMENT_DATA_FILE) {
-        for line in lines {
-            if let Ok(movement) = line {
-                let mut iter = movement.split_whitespace();
-                if let Some(direction) = iter.next() {
-                    if let Some(distance_string) = iter.next() {
-                        if let Ok(distance_value) = distance_string.parse() {
-                            if direction == "forward" {
-                                uboat.forward(distance_value);
-                            } else if direction == "down" {
-                                uboat.down(distance_value);
-                            } else if direction == "up" {
-                                uboat.up(distance_value);
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-    uboat
-}
-
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-    where P: AsRef<Path>, {
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
-}
-
-// for each line in the file after first,
-// compare against the prior line
-// if numbers is greater, increment the count
-// return the count at the end of the file
-fn get_increased_depth() -> i32 {
-    let mut current_window = [-1, -1, -1];
-        let mut position:usize = 0;
-    let mut increased_count = 0;
-    // Day 1 part 1let mut prior_line = -1;
-    let mut prior_sum = -1;
-
-    if let Ok(lines) = read_lines(DEPTH_DATA_FILE) {
-        for line in lines {
-            if let Ok(line_value) = line {
-                if let Ok(value) = line_value.parse() {
-                    current_window[position] = value;
-                    if prior_sum == -1 {
-                        if position == 2 {
-                            prior_sum = current_window.iter().sum();
-                        }
-                    } else {
-                        let current_sum = current_window.iter().sum();
-                        if current_sum > prior_sum {
-                            increased_count += 1;
-                        }
-                        prior_sum = current_sum;
-                    }
-                    if position == 2 {
-                        position = 0;
-                    } else {
-                        position += 1;
-                    }
-                    /* Day Part 1. Done.
-                     if prior_line != -1 {
-                         if prior_line < value {
-                             increased_count += 1;
-                         }
-                     }
-                     prior_line = value;
-
-                     */
-                }
-            }
-        }
-    }
-    increased_count
 }
 
 
--- src/submarine.rs
+++ src/submarine.rs
@@ -1,3 +1,8 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+
 pub struct Submarine {
     pub horizontal_position: i32,
     pub depth: i32,
@@ -9,6 +14,8 @@ pub struct Submarine {
     pub diag_distribution: [i32; 12],
     pub diagnostic_count: i32,
     pub diagnostic_data: Vec<[i32; 12]>,
+    pub bingo_draw: Vec<i32>,
+    pub bingo_boards: Vec<Vec<i32>>,
 }
 
 impl Submarine {
@@ -24,7 +31,97 @@ impl Submarine {
             epsilon: Vec::new(),
             oxygen_rating: Vec::new(),
             co2_scrubber_rating: Vec::new(),
+            bingo_draw: Vec::new(),
+            bingo_boards: vec![vec![0; 5]; 5],
         }
+    }
+
+    pub fn process_diagnostics(&mut self, filename: &str) {
+        if let Ok(lines) = Submarine::read_lines(filename) {
+            for line in lines {
+                if let Ok(bits) = line {
+                    let char_vec: Vec<char> = bits.chars().collect();
+                    self.store_diagnostics(char_vec);
+                }
+            }
+        }
+    }
+
+    pub fn move_sub(&mut self, filename: &str) {
+        if let Ok(lines) = Submarine::read_lines(filename) {
+            for line in lines {
+                if let Ok(movement) = line {
+                    let mut iter = movement.split_whitespace();
+                    if let Some(direction) = iter.next() {
+                        if let Some(distance_string) = iter.next() {
+                            if let Ok(distance_value) = distance_string.parse() {
+                                if direction == "forward" {
+                                    self.forward(distance_value);
+                                } else if direction == "down" {
+                                    self.down(distance_value);
+                                } else if direction == "up" {
+                                    self.up(distance_value);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+        where P: AsRef<Path> {
+        let file = File::open(filename)?;
+        Ok(io::BufReader::new(file).lines())
+    }
+
+    // for each line in the file after first,
+// compare against the prior line
+// if numbers is greater, increment the count
+// return the count at the end of the file
+    pub fn get_increased_depth(&self, filename: &str) -> i32 {
+        let mut current_window = [-1, -1, -1];
+        let mut position:usize = 0;
+        let mut increased_count = 0;
+        // Day 1 part 1let mut prior_line = -1;
+        let mut prior_sum = -1;
+
+        if let Ok(lines) = Submarine::read_lines(filename) {
+            for line in lines {
+                if let Ok(line_value) = line {
+                    if let Ok(value) = line_value.parse() {
+                        current_window[position] = value;
+                        if prior_sum == -1 {
+                            if position == 2 {
+                                prior_sum = current_window.iter().sum();
+                            }
+                        } else {
+                            let current_sum = current_window.iter().sum();
+                            if current_sum > prior_sum {
+                                increased_count += 1;
+                            }
+                            prior_sum = current_sum;
+                        }
+                        if position == 2 {
+                            position = 0;
+                        } else {
+                            position += 1;
+                        }
+                        /* Day Part 1. Done.
+                         if prior_line != -1 {
+                             if prior_line < value {
+                                 increased_count += 1;
+                             }
+                         }
+                         prior_line = value;
+
+                         */
+                    }
+                }
+            }
+        }
+        increased_count
     }
 
     pub fn forward(&mut self, distance: i32) {
